95fec6692ffb204ee2e33824be90ba79a405da98
Lo, B. <bram.lo@syntouch.nl>
2026-05-18 16:20:50 +0200

    chore: initial commit
    Scaffold new .NET solution with an Azure Function app project, containing a sample HTTP trigger.

---
+++ SynTouch.DMNStudio.Hackathon.FunctionApp/MyHttpTrigger.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace SynTouch.DMNStudio.Hackathon.FunctionApp;
+
+public class MyHttpTrigger(ILogger<MyHttpTrigger> logger)
+{
+    [Function("MyHttpTrigger")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
